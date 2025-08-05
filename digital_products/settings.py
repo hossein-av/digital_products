6c6c25212dbdeb4feba76d11629aef8b94fd069d
hossein <hv199977@gmail.com>
2025-08-05 20:26:21 +0330

    product models added.

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles','products'
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+#media files
+MEDIA_ROOt=os.path.join(BASE_DIR,'media/')
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- products/models.py
+++ products/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+#from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+class Category(models.Model):
+
+    parent=models.ForeignKey('self',verbose_name=('parent'),blank=True,null=True,on_delete=models.CASCADE)
+    title=models.CharField(max_length=50)
+    description=models.TextField(blank=True)
+    avatar=models.ImageField(blank=True,upload_to='categories/')
+    is_enable=models.BooleanField(default=True)
+    created_time=models.DateTimeField(auto_now_add=True)
+    upload_time=models.DateTimeField(auto_now=True)
+
+    class meta:
+        db_table='categories'
+        verbose_name=('Category')
+        verbose_name_plural=('Categories')
+
+class Product(models.Model):
+    
+    title=models.CharField(max_length=50)
+    description=models.TextField(blank=True)
+    avatar=models.ImageField(blank=True,upload_to='products/')
+    is_enable=models.BooleanField(default=True)
+    categories=models.ManyToManyField('Category',blank=True,verbose_name=('categories'))
+    created_time=models.DateTimeField(auto_now_add=True)
+    upload_time=models.DateTimeField(auto_now=True)
+
+    class meta:
+        db_table='products'
+        verbose_name=('product')
+        verbose_name_plural=('products')
+
+
+
+class File(models.Model):
+     
+    product=models.ForeignKey('product',on_delete=models.CASCADE,verbose_name=('product'))
+    title=models.CharField(('title'),max_length=50)
+    file=models.FileField(('file'),upload_to='files/%Y/%m/%d/')
+    is_enable=models.BooleanField(('is_enable'),default=True)
+    created_time=models.DateTimeField(('created_time'),auto_now_add=True)
+    upload_time=models.DateTimeField(('upload_time'),auto_now=True)
+    
+    class meta:
+        db_table='files'
+        verbose_name=('file')
+        verbose_name_plural=('files')
